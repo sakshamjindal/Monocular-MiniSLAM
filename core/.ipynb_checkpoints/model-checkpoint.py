83ede3b57044c3ea6b798f9b38aca73ffd9aa9bc
saksham jindal <saksham.jindal@outlook.com>
2020-12-28 03:00:18 +0000

    Added:Dataloader for KITTI dataset

---
+++ core/.ipynb_checkpoints/model-checkpoint.py
@@ -0,0 +1,8 @@
+import cv2
+import numpy as np
+
+
+class VisualOdometry():
+
+    def __init__(self):
+        pass
---
+++ core/geocom/.ipynb_checkpoints/features-checkpoint.py
@@ -0,0 +1,12 @@
+import numpy as np 
+import cv2
+
+
+def featureTracking(image_ref, image_cur, px_ref):
+	kp2, st, err = cv2.calcOpticalFlowPyrLK(image_ref, image_cur, px_ref, None, **lk_params)  #shape: [k,2] [k,1] [k,1]
+
+	st = st.reshape(st.shape[0])
+	kp1 = px_ref[st == 1]
+	kp2 = kp2[st == 1]
+
+	return kp1, kp2
